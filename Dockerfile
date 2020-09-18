e0a0e70078b6b8c90400497955b33d18ec8223ab
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-09-18 14:17:54 +0300

    add templates

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM python:3.8.5
 ENV PORT 8081
-COPY ./requirements.txt /app/requirements.txt
 WORKDIR /app
+COPY ./requirements.txt /app/requirements.txt
 RUN pip install -r requirements.txt
 COPY ./app/*.py /app/
 ENTRYPOINT ["python"]
--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, request, render_template
 from service import fib
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT", 5000))
 
 @app.route('/')
-def hello_world():
-    return '{}'.format(fib(10))
+def index():
+    return render_template('index.html')
+
+@app.route('/fib')
+def fib_web():
+    num = request.args.get('num', default=10, type=int)
+    return '{}-ый член последовательности Фиббоначи равен {}'.format(num, fib(num))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
