34a7bbc4088f3cf0364c4e932fe1f6d529bf01f3
bobruk76 <vladimir.m.polyakov@gmail.com>
2020-09-18 09:49:46 +0300

    new friday

--- app/service.py
+++ app/service.py
@@ -1,17 +1,21 @@
 import json
 import redis
-
 r = redis.StrictRedis(host='localhost', port=6379, db=1)
-r.hmset('fib_memory', json.dumps({1: 1, 2: 1}))
-r.hmset('fib_max', json.dumps(2))
+
+def init_redis():
+    if not r.exists('fib_memory', 'fib_max'):
+        r.set('fib_memory', json.dumps({0: 0, 1: 1}))
+        r.set('fib_max', json.dumps(1))
+
 def fib(num):
-    fib_memory = json.loads(r.hgetall('fib_memory'))
-    fib_max = json.loads(r.hgetall('fib_max'))
-    print(fib_memory)
+    init_redis()
+    fib_memory = json.loads(r.get('fib_memory'))
+
+    fib_max = json.loads(r.get('fib_max'))
     if num <= fib_max:
-        return fib_memory[num]
-    for item in range(fib_memory['max']+1, num+1):
-        fib_memory[item] = fib_memory[item-2] + fib_memory[item-1]
-    r.hmset('fib_max', json.dumps(num))
-    r.hmset('fib_memory', json.dumps(fib_memory))
-    return fib_memory[num]
+        return fib_memory[str(num)]
+    for item in range(fib_max+1, num+1):
+        fib_memory[str(item)] = fib_memory[str(item-2)] + fib_memory[str(item-1)]
+    r.set('fib_max', json.dumps(num))
+    r.set('fib_memory', json.dumps(fib_memory))
+    return fib_memory[str(num)]
